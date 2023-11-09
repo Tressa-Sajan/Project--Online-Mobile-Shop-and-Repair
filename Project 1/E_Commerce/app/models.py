f548cd2faed7fa5d13784e1db538b8302fabe376
Tressa-Sajan <tressasajan2024b@mca.ajce.in>
2023-11-09 22:51:22 +0530

    commit

---
+++ Project 1/E_Commerce/app/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from ckeditor.fields import RichTextField
+
+class User(AbstractUser):
+    userRole = models.CharField(max_length=3, null=True)
+
+# Create your models here.
+class slider(models.Model):
+    DISCOUNT_DEAL = (
+        ('HOT DEALS','HOT DEALS'),
+        ('New Arrivals', 'New Arrivals'),
+    )
+
+
+    Image = models.ImageField(upload_to='media/slider_imgs')
+    Discount_Deal = models.CharField(choices=DISCOUNT_DEAL,max_length=100)
+    SALE = models.IntegerField()
+    Brand_Name = models.CharField(max_length=200)
+    Discount = models.IntegerField()
+    Link = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.Brand_Name
+ 
+class Main_Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    main_category = models.ForeignKey(Main_Category,on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name    + " -- " + self.main_category.name
+
+class Sub_Category(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category.main_category.name + " -- " + self.category.name + " -- " + self.name
+
+# class Section(models.Model):
+    # name = models.CharField(max_length=100, null=True, blank=True)
+
+    # def __str__(self):
+        # return self.name
+
+class Product(models.Model):
+    total_quantity = models.IntegerField(null=True)
+    Availability = models.IntegerField(null=True)
+    featured_image = models.CharField(max_length=100)
+    product_name = models.CharField(max_length=100)
+    price = models.IntegerField(null=True)
+    Discount = models.IntegerField(null=True)
+    Product_information = RichTextField(null=True)
+    model_Name = models.CharField(max_length=100)
+    Categories = models.ForeignKey(Category,on_delete=models.CASCADE)
+    #Tags = models.CharField(max_length=100)
+    Description = RichTextField(null=True)
+    #section = models.ForeignKey(Section,on_delete=models.DO_NOTHING,null=True)
+
+    def __str__(self):
+        return self.product_name
+
+class Product_Image(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    Image_url = models.CharField(max_length=200)
+    
+
+class Additional_Information(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    specification = models.CharField(max_length=100)
+    detail = models.CharField(max_length=100)
--- project/app/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class CustomUser(AbstractUser):
-    address = models.TextField()
-    contact_no = models.CharField(max_length=10)
